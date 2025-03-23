fdad2ac1d7a090aa48a30d6d20274b985d21c6fb
joyboy123-coder <your-email@example.com>
2025-03-23 13:03:28 +0530

    add changes

---
+++ kafka_snowflake_sql/config/snowflake_config.py
@@ -0,0 +1,8 @@
+SNOWFLAKE_CONFIG = {
+    "user":"your_username",
+    "password" : "your_password",
+    "account" : "your_snowflake_account",
+    "warehouse" : "your_warehouse",
+    "database" : "your_database"
+    "schema" : "your_schema" 
+    }
